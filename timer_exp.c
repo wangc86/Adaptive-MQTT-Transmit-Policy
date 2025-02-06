c4115c7f7fe4702c4547cb88d3395a969a8d6b1d
Chao Wang <cw@ntnu.edu.tw>
2025-02-06 08:30:47 +0800

    Update timer_exp.c

--- timer_exp.c
+++ timer_exp.c
@@ -23,11 +23,11 @@ void* lat_thread(void *arg){
 	pthread_exit(NULL);
 }
 
-//這個function裡寫time out時要怎麼處理
+// handle time out
 void timeout_handler(int signum)
 {
     printf("!!!!!!pthread_create!!!!!!\n\n");
-	pthread_t tid; // 宣告 pthread 變數
+	pthread_t tid; // declare variable for pthread 
 	pthread_create(&tid, NULL, lat_thread, NULL);
 	pthread_join(tid, NULL);
     printf("!!!!!!Timeout!!!!!!\n\n");  
@@ -35,12 +35,12 @@ void timeout_handler(int signum)
 
 timer_t create_the_timer(int sig, int sec){
 
-    timer_t timerid;            //定時器ID      
-    struct sigevent sev;        //指定了定時器到期要產生的異步通知
-    struct itimerspec its;      //超時結構體
-    struct sigaction sa;        //超時要觸發的signal
+    timer_t timerid;                  
+    struct sigevent sev;        
+    struct itimerspec its;      
+    struct sigaction sa;        
 
-    sa.sa_handler = timeout_handler;    //time out的處理function
+    sa.sa_handler = timeout_handler;    
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = 0;
     if(sigaction(SIGUSR1, &sa, NULL) == -1)
@@ -52,9 +52,9 @@ timer_t create_the_timer(int sig, int sec){
     if(timer_create(CLOCK_REALTIME, &sev, &timerid) == -1)
         perror("timer_create");
 
-    its.it_value.tv_sec = sec;                      //表示定時器的第一次超時時間
+    its.it_value.tv_sec = sec;                      
     its.it_value.tv_nsec = 0;
-    its.it_interval.tv_sec = its.it_value.tv_sec;   //表示定時器第一次超時以後的超時時間間隔
+    its.it_interval.tv_sec = its.it_value.tv_sec;   
     its.it_interval.tv_nsec = its.it_value.tv_nsec;
 
     if(timer_settime(timerid, 0, &its, NULL) == -1)
